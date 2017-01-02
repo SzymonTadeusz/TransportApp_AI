14a04daa9f51b0f3abf7066867b232b6d8c65e5a
SzymonM <szymon.muszynski@student.wat.edu.pl>
2017-01-02 18:38:11 +0100

    Page layout changes

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -7,26 +7,47 @@
 
     HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController($scope, Principal, LoginService, $state) {
         var vm = this;
+        $scope.myInterval = 8000;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+
+        slides.push({
+                image: "../../content/images/slider/bg1.jpg",
+                title: "Sample title 1",
+                text: "Sample text 1",
+                id: currIndex++
+            },
+            {
+                image: "../../content/images/slider/bg2.jpg",
+                title: "Sample title 2",
+                text: "Sample text 2",
+                id: currIndex++
+            });
+
+        console.log(slides);
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
+        $scope.$on('authenticationSuccess', function () {
             getAccount();
         });
 
         getAccount();
 
         function getAccount() {
-            Principal.identity().then(function(account) {
+            Principal.identity().then(function (account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
             });
         }
-        function register () {
+
+        function register() {
             $state.go('register');
         }
     }
